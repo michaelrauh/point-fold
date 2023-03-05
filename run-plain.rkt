9cad3dec546b9da7f718c71f4f4e76f3d41f5f43
michaelrauh <michaelrauh@ymail.com>
2023-03-05 16:30:06 -0500

    add comments to run plain

--- run-plain.rkt
+++ run-plain.rkt
@@ -8,8 +8,14 @@
 ; combine pruning and finding subtree at path into one
 ; use a tally vector instead of a set intersect over lists
 ; dememoize running rules <- this helps slightly
+; integer encode strings
 
 ; baseline cpu time: 8934 real time: 8765 gc time: 1273
 ; no depth pruning cpu time: 8085 real time: 7311 gc time: 1250
 ; no span pruning cpu time: 146520 real time: 146513 gc time: 31901
 ; no rules memoization: cpu time: 9145 real time: 8752 gc time: 1458
+
+
+; plan:
+; integer encode strings
+; use a tally vector
