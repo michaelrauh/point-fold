4f3cd19edd989441f87380201578d6044fc47179
michaelrauh <michaelrauh@ymail.com>
2023-05-08 18:36:55 -0400

    finish thought - need to go back over WIP

--- bnb.rkt
+++ bnb.rkt
@@ -4,18 +4,42 @@
 (define (run dims vocabulary reference)
   (for/fold ([stack (list (make-blank dims))])
             ([i (in-naturals)])
-        #:break (or (empty? stack) (full? (car stack)))
-    (begin
-      (define top (car stack))
-      (define rest (cdr stack))
-      (define all-to-add (map ((curry add-at-next) top) vocabulary))
-              (define additions (for/fold ([acc stack])
-                        ([to-add all-to-add]
-                         #:when (legal? reference to-add))
-                (cons to-add stack)))
-              (append additions rest))))
+    #:break (or (empty? stack) (full? (car stack)))
+    (append (for/fold ([acc stack])
+                      ([to-add (map ((curry add-at-next) (car stack)) vocabulary)]
+                       #:when (legal? reference to-add))
+              (cons to-add acc)) (cdr stack))))
 
+(define (example)
+  (define f (file->string "example.txt"))
+  (define dims '(2 2))
+  (define vocab (corpus->vocabulary f))
+  (define reference (corpus->reference f))
+  (run dims vocab reference))
 
+(define (corpus->vocabulary corpus)
+  (remove-duplicates (flatten (corpus->sentences corpus))))
+
+(define (corpus->reference corpus)
+  (for/fold ([acc (set)])
+            ([phrase (apply append (map phrases (corpus->sentences corpus)))])
+    (set-add acc phrase)))
+
+(define (phrases l)
+  (apply append (map prefixes (suffixes l))))
+
+(define (suffixes l)
+  (if (= 1 (length l)) (list l) (cons l (suffixes (cdr l)))))
+
+(define (prefixes l)
+  (if (= 1 (length l)) (list l) (cons l (prefixes (drop-right l 1)))))
+
+(define (corpus->sentences corpus)
+  (filter cons? (map string-split (corpus->sentence-strings corpus))))
+
+(define (corpus->sentence-strings corpus)
+  (remove-duplicates
+   (map string-downcase (map string-trim (string-split corpus #px"\\!|\\.|\\?|\\;|\\:")))))
     
 
 ; this is a nice time to preallocate. Perhaps to preallocate lots of these
@@ -44,3 +68,5 @@
   (define all_orthotope_views (map (λ (ax) (array-axis-swap box ax (sub1 (array-dims box)))) (range (array-dims box))))
   (define phrases (map (λ (pos) (array->list* (array-reshape (list-ref all_orthotope_views pos) (list->vector (list (/ (array-size box) (list-ref (array-shape box) pos)) (list-ref (array-shape box) pos)))))) (range (array-dims box))))
   phrases)
+
+(example)
